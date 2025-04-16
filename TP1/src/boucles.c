f5c27828e65ceaaffa772bbce75905945ae7ada9
Shvwn9 <roccashawn08@gmail.com>
2025-04-16 17:23:54 +0200

    Update boucles.c

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 
 int main() {
-    int compteur = 5; // à modifier pour tester d'autres tailles
+    int compteur = 5; 
 
     if (compteur >= 10) {
-        printf("La valeur du compteur doit être inférieure à 10.\n");
+        printf("La valeur du compteur doit être inférieure a 10.\n");
         return 1;
     }
-
     for (int i = 1; i <= compteur; i++) {
         for (int j = 1; j <= i; j++) {
-            // Conditions pour afficher * ou #
             if (i == 1 || i == compteur || j == 1 || j == i) {
                 printf("* ");
             } else {
